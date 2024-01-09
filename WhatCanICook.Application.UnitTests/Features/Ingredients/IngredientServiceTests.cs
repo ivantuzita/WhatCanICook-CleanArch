37304a170f95a043dce44e8d38f3e6a64311bcfc
Ivan Tuzita <ivantuzita@gmail.com>
2024-01-09 11:02:42 -0300

    more unit testing

--- WhatCanICook.Application.UnitTests/Features/Ingredients/IngredientServiceTests.cs
+++ WhatCanICook.Application.UnitTests/Features/Ingredients/IngredientServiceTests.cs
@@ -3,9 +3,11 @@ using Moq;
 using Shouldly;
 using WhatCanICook.Application.MappingProfiles;
 using WhatCanICook.Application.Services;
+using WhatCanICook.Application.DTOs;
 using WhatCanICook.Application.UnitTests.Mocks;
 using WhatCanICook.Domain.Interfaces;
 using WhatCanICook.Domain.Models;
+
 
 namespace WhatCanICook.Application.UnitTests.Features.Ingredients; 
 public class IngredientServiceTests {
@@ -41,5 +43,39 @@ public class IngredientServiceTests {
         exists.ShouldBeOfType<bool>();
         exists.ShouldBeTrue();
     }
+
+    [Fact]
+    public async Task CreateAsyncTest() {
+        var _service = new IngredientService(_mockRepo.Object, _mapper);
+        var ing = new IngredientDTO { Id = 4, Name = "test Onion" };
+
+        await _service.CreateAsync(ing);
+    }
+
+    [Fact]
+    public async Task UpdateAsyncTest() {
+        var _service = new IngredientService(_mockRepo.Object, _mapper);
+        var ing = new IngredientDTO { Id = 1, Name = "test Onion testing" };
+
+        await _service.UpdateAsync(ing);
+        var getAll = await _service.GetAllAsync();
+
+        getAll.Find(u => u.Id == ing.Id).Name.ShouldBe(ing.Name);
+    }
+
+    [Fact]
+    public async Task DeleteByIdTest() {
+        var _service = new IngredientService(_mockRepo.Object, _mapper);
+
+        var getAll = await _service.GetAllAsync();
+
+        await _service.DeleteByIdAsync(3);
+
+        getAll.Count.ShouldBe(2);
+    }
+
+    
+
+
 
 }
--- WhatCanICook.Application.UnitTests/Mocks/MockIngredientRepository.cs
+++ WhatCanICook.Application.UnitTests/Mocks/MockIngredientRepository.cs
@@ -2,7 +2,7 @@
 using WhatCanICook.Domain.Interfaces;
 using WhatCanICook.Domain.Models;
 
-namespace WhatCanICook.Application.UnitTests.Mocks; 
+namespace WhatCanICook.Application.UnitTests.Mocks;
 public class MockIngredientRepository {
     public static Mock<IGenericRepository<Ingredient>> GetMockIngredientsRepository() {
         var ingredients = new List<Ingredient> {
@@ -39,6 +39,30 @@ public class MockIngredientRepository {
                 }
                 return false;
             });
+
+        mockRepo.Setup(r => r.DeleteByIdAsync(It.IsAny<int>()))
+            .Returns((int id) => {
+                var obj = ingredients.Find(u => u.Id == id);
+
+                if (obj != null) {
+                    ingredients.Remove(obj);
+                    return Task.CompletedTask;
+                }
+                //is this correct?
+                return Task.FromResult(false);
+            });
+
+        mockRepo.Setup(r => r.UpdateAsync(It.IsAny<Ingredient>()))
+            .Returns((Ingredient ing) => {
+                var obj = ingredients.Find(u => u.Id == ing.Id);
+                if (obj != null) {
+                    obj = ing;
+                    return Task.CompletedTask;
+                }
+                //is this correct?
+                return Task.FromResult(false);
+            });
+
 
         return mockRepo;
     }
